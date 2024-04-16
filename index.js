882b37c510546ab8398930b9ef5e69940176a8d4
seccret404 <edwardtua25@gmail.com>
2024-04-16 20:48:09 +0700

    piuhs

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const User = require('./models/user');
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000, () => console.log('App listening on port 3000!'));
