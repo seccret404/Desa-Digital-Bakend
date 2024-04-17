b2ca61076006d613552d4625ddff8d55b0f207ee
seccret404 <edwardtua25@gmail.com>
2024-04-17 09:28:45 +0700

    Update Bee

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
 const express = require('express');
 require('dotenv').config();
-const mysql = require('mysql2');
+const pool = require('./config/database'); // Sesuaikan path ke file database
 
 const app = express();
+app.use(express.json());
 
-// Konfigurasi Database
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Endpoint untuk mengambil semua data dari tabel 'dusun'
+app.get('/dusun', (req, res) => {
+  pool.query('SELECT * FROM dusun', (err, results) => {
+    if (err) {
+      console.error('Error when selecting from dusun:', err);
+      res.status(500).send('Error retrieving data from the database');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-
-// Menguji koneksi database sebelum menjalankan server
-pool.query('SELECT 1 + 1 AS solution', (error, results) => {
-  if (error) {
-    console.error('Unable to connect to the database:', error);
-    return; // Jangan menjalankan server jika koneksi database gagal
-  }
-  console.log('Database connection has been established successfully.');
-  // Mulai server hanya jika koneksi database berhasil
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
