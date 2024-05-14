a01ff0a48161158ccbb9a630215f2299497c58d0
seccret404 <edwardtua25@gmail.com>
2024-05-15 06:50:06 +0700

    Update Config

--- config/database.js
+++ config/database.js
@@ -14,14 +14,39 @@
 // module.exports = pool;
 
 
+// const mysql = require('mysql2');
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'desa_digital'
+// });
+
+// module.exports = connection;
+
 const mysql = require('mysql2');
 
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'desa_digital'
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = connection;
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+  connection.release();
+});
+
+module.exports = pool;
+
 
