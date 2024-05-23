121441538de33134a2f856d4ec36894c35aee819
seccret404 <edwardtua25@gmail.com>
2024-05-23 22:47:25 +0700

    Create Auth Feature

--- config/database.js
+++ config/database.js
@@ -1,52 +1,36 @@
 // const mysql = require('mysql2');
 
-// const pool = mysql.createPool({
-//   host: process.env.MYSQL_HOST,
-//   port: process.env.MYSQL_PORT,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'desa_digital'
 // });
 
-// module.exports = pool;
-
+// module.exports = connection;
 
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'desa_digital'
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+connection.getConnection((err, connection) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+  connection.release();
 });
 
 module.exports = connection;
-
-// const mysql = require('mysql2');
-
-// const connection = mysql.createPool({
-//   host: process.env.MYSQL_HOST,
-//   port: process.env.MYSQL_PORT,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
-
-// connection.getConnection((err, connection) => {
-//   if (err) {
-//     console.error('Database connection failed:', err.stack);
-//     return;
-//   }
-//   console.log('Connected to database.');
-//   connection.release();
-// });
-
-// module.exports = connection;
 
 
