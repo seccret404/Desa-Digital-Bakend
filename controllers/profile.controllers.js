25bcd4de40a73cf5342e2e46721fafbe79a655ad
seccret404 <edwardtua25@gmail.com>
2024-06-01 06:04:55 +0700

    Update Profile

--- controllers/profile.controllers.js
+++ controllers/profile.controllers.js
@@ -1,116 +1,103 @@
-const Profil = require('../models/profildesa')
+const Profil = require('../models/profil');
 
-exports.findAllProfile = (req, res)=>{
-     Profil.findAll((err, data) =>{
-          if(err){
-               res.status(500).send({
-                    message: err.message || "Terjadi Kesalahan meganbil data"
-               });
-          }else{
-               res.send(data);
-          }
-     });
-}
+exports.findAllProfile = (req, res) => {
+    Profil.findAll((err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Terjadi kesalahan dalam mengambil data"
+            });
+        } else {
+            res.send(data);
+        }
+    });
+};
 
-exports.createProfile = (req, res)=>{
-     console.log("Data yang dikirimkan : ", req.body);
-     console.log("Data yang dikirimkan: ", req.files);
+exports.createProfile = (req, res) => {
+    const newProfile = new Profil(
+        req.body.nama_desa,
+        req.body.alamat_kantor,
+        req.body.kecamatan,
+        req.body.kabupaten,
+        req.body.provinsi,
+        req.body.profil_singkat,
+        req.body.gambar_desa,
+        req.body.batas_barat,
+        req.body.batas_timur,
+        req.body.batas_utara,
+        req.body.batas_selatan,
+        req.body.visi_desa,
+        req.body.misi_desa,
+        req.body.sejarah_desa
+    );
 
-     if (!req.files || !req.files['gambar_desa'] || !req.files['gambar_desa'][0]) {
-          return res.status(400).send({
-              message: "Gambar desa tidak ditemukan"
-          });
-      }
-
-     const newProfile = new Profil(
-          req.body.nama_desa,
-          req.body.alamat_kantor,
-          req.body.kecamatan,
-          req.body.kabupaten,
-          req.body.provinsi,
-          req.body.profil_singkat,
-          req.files['gambar_desa'][0].filename,
-          req.body.batas_barat,
-          req.body.batas_timur,
-          req.body.batas_utara,
-          req.body.batas_selatan,
-          req.body.visi_desa,
-          req.body.misi_desa,
-          req.body.sejarah_desa
-     );
-
-     Profil.create(newProfile,(err,data)=>{
-          if(err){
-               res.status(500).send({
-                    message: err.message || "Error add data"
-               });
-
-          }else{
-               res.send(data);
-          }
-     })
-}
+    Profil.create(newProfile, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Gagal membuat data profil"
+            });
+        } else {
+            res.send(data);
+        }
+    });
+};
 
 exports.findProfilById = (req, res) => {
-     Profil.findById(req.params.id, (err, data) => { 
-         if (err) {
-             if (err.kind === "not_found") {
-                 res.status(404).send({
-                     message: `Profile dengan id ${req.params.id} tidak ditemukan.`
-                 });
-             } else {
-                 res.status(500).send({
-                     message: `Profile retrieving Berita with id ${req.params.id}`
-                 });
-             }
-         } else {
-             res.send(data);
-         }
-     });
- };
+    Profil.findById(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Profile dengan id ${req.params.id} tidak ditemukan.`
+                });
+            } else {
+                res.status(500).send({
+                    message: `Error retrieving profile with id ${req.params.id}`
+                });
+            }
+        } else {
+            res.send(data);
+        }
+    });
+};
 
- 
 exports.editProfil = (req, res) => {
-     console.log("Data yang diterima untuk diedit:", req.body);
-     console.log("Data yang dikirim:", req.files);
- 
-     if (!req.files || !req.files['gambar_desa'] || !req.files['gambar_desa'][0]) {
-          return res.status(400).send({
-              message: "Gambar desa tidak ditemukan"
-          });
-      }
-   
-      const { gambar_desa } = req.files || {};
-      const newProfile = new Profil(
-          req.body.nama_desa,
-          req.body.alamat_kantor,
-          req.body.kecamatan,
-          req.body.kabupaten,
-          req.body.provinsi,
-          req.body.profil_singkat,
-          gambar_desa[0].filename,
-          req.body.batas_barat,
-          req.body.batas_timur,
-          req.body.batas_utara,
-          req.body.batas_selatan,
-          req.body.visi_desa,
-          req.body.misi_desa,
-          req.body.sejarah_desa
-     );
- 
-     Profil.update(req.params.id, newProfile, (err, data) => {
-         if (err) {
-             if (err.kind === "not_found") {
-                 res.status(404).send({
-                     message: `Profile dengan id ${req.params.id} tidak ditemukan.`
-                 });
-             } else {
-                 res.status(500).send({
-                     message: `Error updating profile with id ${req.params.id}`
-                 });
-             }
-         } else {
-             res.send(data);
-         }
-     });
- };
+    const id = req.params.id;
+
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update cannot be empty!"
+        });
+    }
+
+    const profil = new Profil(
+        req.body.nama_desa,
+        req.body.alamat_kantor,
+        req.body.kecamatan,
+        req.body.kabupaten,
+        req.body.provinsi,
+        req.body.profil_singkat,
+        req.body.gambar_desa,
+        req.body.batas_barat,
+        req.body.batas_timur,
+        req.body.batas_utara,
+        req.body.batas_selatan,
+        req.body.visi_desa,
+        req.body.misi_desa,
+        req.body.sejarah_desa
+    );
+
+    Profil.update(id, profil, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Profile dengan id ${id} tidak ditemukan.`
+                });
+            } else {
+                res.status(500).send({
+                    message: `Error updating profile with id ${id}`
+                });
+            }
+        } else {
+            res.send(data);
+        }
+    });
+};
--- models/profildesa.js
+++ models/profildesa.js
@@ -1,86 +1,77 @@
-const db = require('../config//database')
+const db = require('../config/database');
 
-class Profil{
-     constructor(nama_desa,alamat_kantor,kecamatan,kabupaten,provinsi,profil_singkat,gambar_desa,batas_barat,batas_timur,batas_utara,batas_selatan,visi_desa,misi_desa,sejarah_desa,createdAt = new Date(),updatedAt = new Date(),){
-          this.nama_desa = nama_desa;
-          this.alamat_kantor = alamat_kantor;
-          this.kecamatan = kecamatan;
-          this.kabupaten = kabupaten;
-          this.provinsi = provinsi;
-          this.profil_singkat = profil_singkat;
-          this.gambar_desa = gambar_desa;
-          this.batas_barat = batas_barat;
-          this.batas_timur = batas_timur;
-          this.batas_utara = batas_utara;
-          this.batas_selatan = batas_selatan;
-          this.visi_desa = visi_desa;
-          this.misi_desa = misi_desa;
-          this.sejarah_desa=sejarah_desa;
-          this.createdAt = createdAt;
-          this.updatedAt = updatedAt
-     }
+class Profil {
+    constructor(nama_desa, alamat_kantor, kecamatan, kabupaten, provinsi, profil_singkat, gambar_desa, batas_barat, batas_timur, batas_utara, batas_selatan, visi_desa, misi_desa, sejarah_desa, createdAt = new Date(), updatedAt = new Date()) {
+        this.nama_desa = nama_desa;
+        this.alamat_kantor = alamat_kantor;
+        this.kecamatan = kecamatan;
+        this.kabupaten = kabupaten;
+        this.provinsi = provinsi;
+        this.profil_singkat = profil_singkat;
+        this.gambar_desa = gambar_desa;
+        this.batas_barat = batas_barat;
+        this.batas_timur = batas_timur;
+        this.batas_utara = batas_utara;
+        this.batas_selatan = batas_selatan;
+        this.visi_desa = visi_desa;
+        this.misi_desa = misi_desa;
+        this.sejarah_desa = sejarah_desa;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 
-     static findAll(result){
-          db.query("SELECT * FROM profiles",(err, res)=>{
-               if(err){
-                    result(err,null);
-                         return;
-               }  
-               result(null,res)
-          });
-     }
+    static findAll(result) {
+        db.query("SELECT * FROM profiles", (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            }
+            result(null, res);
+        });
+    }
 
-     static create(newProfile, result){
-          console.log("Data yang dikirimkan: ", newProfile);
-          db.query("INSERT INTO profiles SET ?", newProfile,(err,res)=>{
-               if(err){
-                    result(err,null);
+    static create(newProfile, result) {
+        console.log("Data yang dikirimkan: ", newProfile);
+        db.query("INSERT INTO profiles SET ?", newProfile, (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            }
+            result(null, { id: res.insertId, ...newProfile });
+        });
+    }
+
+    static findById(id, result) {
+        db.query("SELECT * FROM profiles WHERE id = ?", [id], (err, res) => {
+            if (err) {
+                result(err, null);
+                return;
+            }
+            if (res.length) {
+                result(null, res[0]);
+                return;
+            }
+            result({ kind: "not_found" }, null);
+        });
+    }
+
+    static update(id, profil, result) {
+        db.query(
+            "UPDATE profiles SET nama_desa = ?, alamat_kantor = ?, kecamatan = ?, kabupaten = ?, provinsi = ?, profil_singkat = ?, gambar_desa = ?, batas_barat = ?, batas_timur = ?, batas_utara = ?, batas_selatan = ?, visi_desa = ?, misi_desa = ?, sejarah_desa = ?, updatedAt = ? WHERE id = ?",
+            [profil.nama_desa, profil.alamat_kantor, profil.kecamatan, profil.kabupaten, profil.provinsi, profil.profil_singkat, profil.gambar_desa, profil.batas_barat, profil.batas_timur, profil.batas_utara, profil.batas_selatan, profil.visi_desa, profil.misi_desa, profil.sejarah_desa, new Date(), id],
+            (err, res) => {
+                if (err) {
+                    result(err, null);
                     return;
-               }
-               result(null, {id: res.insertId, ...newProfile});
-          });
-     }
-
-     static findById(id, result) {
-          db.query(`SELECT * FROM profiles WHERE id = ${id}`, (err, res) => {
-              if (err) {
-                  result(err, null);
-                  return;
-              }
-  
-              if (res.length) {
-                  result(null, res[0]);
-                  return;
-              }
-  
-              result({ message: "Agenda not found" }, null);
-          });
-      }
-      
-
-      static update(id, profil, result) {
-          db.query(
-              "UPDATE profiles SET nama_desa = ?, alamat_kantor = ?, kecamatan = ?, provinsi = ?, profil_singkat = ?, gambar_desa = ?, batas_barat = ?, batas_timur = ?, batas_utara = ?, batas_selatan = ?, visi_desa = ?, misi_desa = ?, sejarah_desa = ? WHERE id = ?",
-              [profil.nama_desa, profil.alamat_kantor, profil.kecamatan, profil.provinsi, profil.profil_singkat, profil.gambar_desa, profil.batas_barat, profil.batas_timur, profil.batas_utara, profil.batas_selatan, profil.visi_desa, profil.misi_desa, profil.sejarah_desa, id],
-              (err, res) => {
-                  if (err) {
-                      result(err, null);
-                      return;
-                  }
-      
-                  if (res.affectedRows == 0) {
-                      // profil tidak ditemukan dengan id yang diberikan
-                      result({ kind: "not_found" }, null);
-                      return;
-                  }
-      
-                  console.log("profil updated: ", { id: id, ...profil });
-                  result(null, { id: id, ...profil });
-              }
-          );
-      }
-      
-
+                }
+                if (res.affectedRows == 0) {
+                    result({ kind: "not_found" }, null);
+                    return;
+                }
+                result(null, { id: id, ...profil });
+            }
+        );
+    }
 }
 
 module.exports = Profil;
