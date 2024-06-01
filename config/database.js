45bbf817c4f3fc918ca7e1ec6548af6c8d19978c
seccret404 <edwardtua25@gmail.com>
2024-06-02 00:07:43 +0700

    Update

--- config/database.js
+++ config/database.js
@@ -11,31 +11,28 @@
 const mysql = require('mysql2');
 const { initializeAdmin } = require('../models/auth');
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: process.env.MYSQL_DATABASE
 });
 
-connection.promise().then((pool) => {
-  pool.query('SELECT 1').then(() => {
-    console.log('Connected to database.');
-
-    addUser('admin', 'adminpassword').then(() => {
-      console.log('Admin initialized');
-    }).catch((err) => {
-      console.error('Error initializing admin:', err);
-    });
-  }).catch((err) => {
-    console.error('Database connection failed:', err);
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+  
+  addUser('admin', 'adminpassword').then(() => {
+    console.log('Admin initialized');
+  }).catch(err => {
+    console.error('Error initializing admin:', err);
   });
-}).catch((err) => {
-  console.error('Database connection failed:', err);
+
+  connection.end();
 });
 
 module.exports = connection;
