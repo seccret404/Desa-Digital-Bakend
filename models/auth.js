897d220fe4fde02ef1191194ef0fa5fb975ebf61
seccret404 <edwardtua25@gmail.com>
2024-06-01 23:59:54 +0700

    Update

--- models/auth.js
+++ models/auth.js
@@ -5,16 +5,12 @@ const addUser = async (username, password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = { username, password: hashedPassword };
 
-  connection.promise().then((pool) => {
-    pool.query('INSERT INTO admin (username, password) VALUES (?, ?)', [user.username, user.password], (error, results, fields) => {
-      if (error) {
-        console.error('Error adding user to database:', error);
-        return;
-      }
-      console.log('User added to database');
-    });
-  }).catch((error) => {
-    console.error('Error getting database connection:', error);
+  connection.query('INSERT INTO admin (username, password) VALUES (?, ?)', [user.username, user.password], (error, results, fields) => {
+    if (error) {
+      console.error('Error adding user to database:', error);
+      return;
+    }
+    console.log('User added to database');
   });
 
   return user;
