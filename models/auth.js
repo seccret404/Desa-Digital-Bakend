dc8ca121289c5edba653e9c2d81c2a73d24af574
seccret404 <edwardtua25@gmail.com>
2024-06-01 23:53:41 +0700

    Update

--- models/auth.js
+++ models/auth.js
@@ -5,20 +5,27 @@ const addUser = async (username, password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = { username, password: hashedPassword };
 
-  connection.query('INSERT INTO admin (username, password) VALUES (?, ?)', [user.username, user.password], (error, results, fields) => {
-    if (error) {
-      console.error('Error adding user to database:', error);
+  connection.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting database connection:', err);
       return;
     }
-    console.log('User added to database');
+
+    connection.query('INSERT INTO admin (username, password) VALUES (?, ?)', [user.username, user.password], (error, results, fields) => {
+      connection.release(); // Selalu ingat untuk melepaskan koneksi setelah penggunaan
+      if (error) {
+        console.error('Error adding user to database:', error);
+        return;
+      }
+      console.log('User added to database');
+    });
   });
 
   return user;
 };
 
-
 const initializeAdmin = async () => {
-  await addUser('desaku', 'desaupdate'); // Tambahkan admin awal saat database terkoneksi
+  await addUser('admin', 'adminpassword'); // Tambahkan admin awal saat database terkoneksi
 };
 
 module.exports = { addUser, initializeAdmin };
